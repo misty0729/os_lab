90fe3f18cbf38fe336579d341161e16918b2273c
misty0729 <qianz_need@163.com>
2020-03-29 18:02:16 +0800

    完成移植

--- ucore_os_lab-master/labcodes/lab3/kern/debug/kdebug.c
+++ ucore_os_lab-master/labcodes/lab3/kern/debug/kdebug.c
@@ -293,17 +293,33 @@ read_eip(void) {
  * */
 void
 print_stackframe(void) {
-     /* LAB1 YOUR CODE : STEP 1 */
+     /* LAB1 2015010207 : STEP 1 */
      /* (1) call read_ebp() to get the value of ebp. the type is (uint32_t);
-      * (2) call read_eip() to get the value of eip. the type is (uint32_t);
-      * (3) from 0 .. STACKFRAME_DEPTH
-      *    (3.1) printf value of ebp, eip
-      *    (3.2) (uint32_t)calling arguments [0..4] = the contents in address (uint32_t)ebp +2 [0..4]
-      *    (3.3) cprintf("\n");
-      *    (3.4) call print_debuginfo(eip-1) to print the C calling function name and line number, etc.
-      *    (3.5) popup a calling stackframe
-      *           NOTICE: the calling funciton's return addr eip  = ss:[ebp+4]
-      *                   the calling funciton's ebp = ss:[ebp]
-      */
+     */
+     uint32_t ebp = read_ebp();
+      // (2) call read_eip() to get the value of eip. the type is (uint32_t);
+      uint32_t eip = read_eip();
+      // (3) from 0 .. STACKFRAME_DEPTH
+      int i = 0;
+      while(i < STACKFRAME_DEPTH && ebp != 0 && eip != 0) {
+          // (3.1) printf value of ebp, eip
+           cprintf("ebp:0x%08x eip:0x%08x args:", ebp, eip);   
+           // (uint32_t)calling arguments [0..4] = the contents in address (uint32_t)ebp +2 [0..4]
+            uint32_t *args = (uint32_t *)ebp + 2;                            
+            int j = 0;
+            while(j < 4){           
+                cprintf("0x%08x ", args[j]);
+                j ++;
+            }
+            //(3.3) cprintf("\n");
+            cprintf("\n");   
+            // (3.4) call print_debuginfo(eip-1) to print the C calling function name and line number, etc.
+            print_debuginfo(eip - 1);
+            //(3.5) popup a calling stackframe
+            eip = ((uint32_t *)ebp)[1]; 
+            ebp = ((uint32_t *)ebp)[0]; 
+            i++;
+      }
 }
+
 
